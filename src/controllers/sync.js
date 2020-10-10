51441e161ebf900d09c6ab37715fe67bfb9d8b91
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-10 11:23:08 +0200

    add workspaces & projects route

--- src/controllers/sync.js
+++ src/controllers/sync.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const Users = require('../modules/users');
+const Workspaces = require('../modules/workspaces');
+const Projects = require('../modules/projects');
 const logger = require('../utils/logger');
 
 const router = express.Router();
@@ -14,10 +16,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/workspaces', async (req, res) => {
+  try {
+    // refresh token
+    res.status(200).send({ success: true, data: await Workspaces.get(req.query.accessToken) });
+  } catch (e) {
+    logger.error(__filename, 'sync /workspaces', e.message);
+    res.status(400).json({ success: false, message: e.message });
+  }
+});
+
 router.get('/users', async (req, res) => {
   try {
-    // refresh token + get users + (format? + response users
+    // refresh token
     res.status(200).send({ success: true, data: await Users.get(req.query.accessToken) });
+  } catch (e) {
+    logger.error(__filename, 'sync /users', e.message);
+    res.status(400).json({ success: false, message: e.message });
+  }
+});
+
+router.get('/projects', async (req, res) => {
+  try {
+    // refresh token
+    res.status(200).send({ success: true, data: await Projects.get(req.query.accessToken) });
   } catch (e) {
     logger.error(__filename, 'sync /users', e.message);
     res.status(400).json({ success: false, message: e.message });
