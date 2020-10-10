67ab3d61fa553dd78c049ed4835c2b5c786dc6a7
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-10 11:23:40 +0200

    format users

--- src/modules/users.js
+++ src/modules/users.js
@@ -2,8 +2,17 @@ const Asana = require('./request/asana');
 
 const get = async (accessToken) => {
   const users = await Asana.users(accessToken);
-  // format
-  return users;
+
+  return users.data.filter(user => user.email && user.name).map(user => {
+    const [firstname, lastname] = user.name.split(' ');
+    return {
+      asanaId: user.gid,
+      email: user.email,
+      firstname,
+      lastname,
+      photo: user.photo && (user.photo.image_128x128 || user.photo.image_60x60),
+    };
+  });
 };
 
 exports.get = get;
