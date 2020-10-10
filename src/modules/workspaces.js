7d9f16aa5b0d84178242324f20b66ad687d3911f
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-10 11:23:28 +0200

    get & format projects

---
+++ src/modules/workspaces.js
@@ -0,0 +1,16 @@
+const Asana = require('./request/asana');
+
+const get = async (accessToken) => {
+  const workspaces = await Asana.workspaces(accessToken);
+
+  return workspaces.data.map(workspace => {
+    return {
+      asanaId: workspace.gid,
+      name: workspace.name,
+      emailDomains: workspace.email_domains,
+      isOrganization: workspace.is_organization,
+    };
+  });
+};
+
+exports.get = get;
