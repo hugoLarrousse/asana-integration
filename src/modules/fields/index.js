34bbeeeffc69e588cac512c15d56dd9470133a48
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-10 11:24:24 +0200

    control fields we get from asana API

---
+++ src/modules/fields/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable max-len */
+module.exports = {
+  workspaces: 'resource_type,name,email_domains,is_organization',
+  users: 'name,email,photo,workspaces.name,photo',
+  projects: 'name,color,created_at,current_status,custom_field_settings,due_on,members,modified_at,public,start_on,workspace,custom_fields,followers,icon,owner,team',
+};
