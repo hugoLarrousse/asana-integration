7bc4f1ed1fa0f99024ecb301a583a4a444c9a585
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-12 16:57:15 +0200

    update fields for projects

--- src/modules/fields/index.js
+++ src/modules/fields/index.js
@@ -2,6 +2,6 @@
 module.exports = {
   workspaces: 'resource_type,name,email_domains,is_organization',
   users: 'name,email,photo,workspaces.name,photo',
-  projects: 'name,color,created_at,current_status,custom_field_settings,due_on,members,modified_at,public,start_on,workspace,custom_fields,followers,icon,owner,team',
+  projects: 'name,color,created_at,current_status,due_on,members,modified_at,public,start_on,workspace,icon,owner,team',
   tasks: 'name',
 };
