c6dd0b13a4fc8990146843db5bf8fa4d4b8bd047
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-19 19:19:00 +0200

    workspace field

--- src/modules/fields/index.js
+++ src/modules/fields/index.js
@@ -3,5 +3,5 @@ module.exports = {
   workspaces: 'resource_type,name,email_domains,is_organization',
   users: 'name,email,photo,workspaces.name,photo',
   projects: 'name,color,created_at,current_status,due_on,members,modified_at,public,start_on,workspace,icon,owner,team',
-  tasks: 'name,completed,completed_at,completed_by,created_at,due_at,due_on,modified_at,num_likes,start_on,assignee,parent',
+  tasks: 'name,completed,completed_at,completed_by,created_at,due_at,due_on,modified_at,num_likes,start_on,assignee,parent,workspace',
 };
