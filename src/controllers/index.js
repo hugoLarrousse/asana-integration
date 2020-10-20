261928d6e0a18ebc2c902688aa156e7ff48207d4
Hugo Larrousse <hugo.larrousse@gmail.com>
2020-10-20 11:54:01 +0200

    webhooks routes is enable

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,16 +4,14 @@ const middleware = require('../utils/middleware');
 
 const oauthController = require('./oauth');
 const syncController = require('./sync');
-// const webhooksController = require('./webhooks');
+const webhooksController = require('./webhooks');
 
 const router = express.Router();
 
-// router.use('/webhooks', webhooksController);
-
 router.use('/', middleware.verifyToken);
 
+router.use('/webhooks', webhooksController);
 router.use('/oauth', oauthController);
-
 router.use('/sync', syncController);
 
 module.exports = router;
